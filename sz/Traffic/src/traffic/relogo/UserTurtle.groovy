f0f7c716118df881be94dfc96cf7ed1aee4123ab
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-11-22 15:45:54 +0100

    refactor UserTurtle.step

--- sz/Traffic/src/traffic/relogo/UserTurtle.groovy
+++ sz/Traffic/src/traffic/relogo/UserTurtle.groovy
@@ -13,8 +13,9 @@ import traffic.ReLogoTurtle;
 class UserTurtle extends ReLogoTurtle{
 	enum State { ACCELERATING, BRAKING, DRIVING }
 	
-	long createdAt
-	def speed = 0
+	public long createdAt
+	static deceleration = 75
+	def speed = 0.0
 	def state = State.ACCELERATING
 	Destination destination = null
 	def shouldDestroy = false
@@ -28,49 +29,71 @@ class UserTurtle extends ReLogoTurtle{
 		numAllCars++
 		createdAt = System.currentTimeMillis()
 	}
+	
+	def brake() {
+		state = State.BRAKING
+	}
+	
+	def accelerate() {
+		state = State.ACCELERATING
+	}
+	
+	def drive() {
+		state = State.DRIVING
+	}
 
 	def step(double dt) {
-		def carsAhead = inCone(userTurtles(), 4, 70).size()
-		if (carsAhead >= 1) {
-			state = State.BRAKING
-			setLabel(carsAhead + " car!")
-		} else {
-			state = State.ACCELERATING
-		}
+		brakeIfCarsAhead()
+		// giveWay()
 		
-		if (speed < maxSpeed && state == State.ACCELERATING) {
+		move(dt)
+		
+		turnToDestination()
+		detectCollisions()
+		dieIfDestroyed()
+	}
+	
+	def brakeIfCarsAhead() {
+		def cars = userTurtles()
+		cars.remove(this)
+		def carsAhead = inCone(cars, 4, 70)
+		if (carsAhead.size() >= 1) {
+			brake()
+			setLabel("$carsAhead")
+		} else {
+			accelerate()
+			setLabel('free way!')
+		}
+	}
+	
+	def giveWay() {
+		if (shouldYield()) {
+			brake()
+		} else if (state == State.BRAKING) {
+			accelerate()
+		}
+	}
+	
+	def move(double dt) {
+		def preSpeed = speed
+		if (state == State.ACCELERATING) {
 			speed += acceleration * dt
 		} else if (speed >= 0 && state == State.BRAKING) {
-			speed -= acceleration * dt
+			speed -= deceleration * dt
 		}
 		speed = Math.max(Math.min(speed, maxSpeed), 0)
-		
-		if (state == State.ACCELERATING && speed >= maxSpeed) {
-			state = State.DRIVING
-		}
-		
+		forward(speed * dt)
+	}
+	
+	def turnToDestination() {
 		def threshold = 0.8
 		
 		if ((Math.abs(getXcor() - destination.getXcor()) < threshold) ^ (Math.abs(getYcor() - destination.getYcor()) < threshold)) {
 			face(destination)
 		}
-		
-		if (shouldYield()) {
-			setLabel("giving way")
-			state = State.BRAKING
-			speed = 0
-		} else if (state == State.BRAKING) {
-			setLabel()
-			state = State.ACCELERATING
-		}
-
-		if (shouldDestroy) {
-			die()
-			return
-		}
-		
-		forward(speed * dt)
-		
+	}
+	
+	def detectCollisions() {
 		def collisions = userTurtlesHere()
 		if (collisions.size() > 1) {
 			ask(collisions) {
@@ -78,6 +101,13 @@ class UserTurtle extends ReLogoTurtle{
 				numCrashes += 1
 				destroy()
 			}
+			return
+		}
+	}
+	
+	def dieIfDestroyed() {
+		if (shouldDestroy) {
+			die()
 			return
 		}
 		
